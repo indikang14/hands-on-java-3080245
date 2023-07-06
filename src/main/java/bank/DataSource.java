ed5e4efc2f8fa92a29b3368a84576560ded3373c
indikang14 <32300383+indikang14@users.noreply.github.com>
2023-07-06 19:30:51 +0000

    get Customer object

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -20,8 +22,43 @@ public class DataSource {
 
   }
 
+  public static Customer getCustomer(String username) {
+    String sql = "Select * FROM Customers WHERE USERNAME  = ? ";
+    Customer customer = null;
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, username);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        customer = new Customer(resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
+
+        System.out.println(username + " fields: ");
+
+        System.out.println(customer.getId());
+        System.out.println(customer.getName());
+        System.out.println(customer.getUsername());
+        System.out.println(customer.getAccountId());
+        System.out.println(customer.getPwd());
+
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return customer;
+
+  }
+
   public static void main(String[] args) {
-    connect();
+    // connect();
+    String username = "lfromonte9@de.vu";
+    getCustomer(username);
 
   }
 }
