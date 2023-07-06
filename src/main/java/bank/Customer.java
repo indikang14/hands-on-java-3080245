63465215ebd4439db9a0367535a946ee06066ff5
indikang14 <32300383+indikang14@users.noreply.github.com>
2023-07-06 18:23:04 +0000

    new customer class

---
+++ src/main/java/bank/Customer.java
@@ -0,0 +1,58 @@
+package bank;
+
+public class Customer {
+  private int id;
+  private String name;
+  private String username;
+  private String pwd;
+  private int accountId;
+
+  public Customer(int id, String name, String username, String pwd, int accountId) {
+    setId(id);
+    setName(name);
+    setUsername(username);
+    setPwd(pwd);
+    setAccountId(accountId);
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPwd() {
+    return this.pwd;
+  }
+
+  public void setPwd(String pwd) {
+    this.pwd = pwd;
+  }
+
+  public int getAccountId() {
+    return this.accountId;
+  }
+
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+
+}
